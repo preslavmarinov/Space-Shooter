a66b4dea195d36b255483ec0cc4bd136e4fd683f
preslavmarinov <preslavivanovmarinov@gmail.com>
2024-12-16 14:35:16 +0200

    improve rocket movement

--- Assets/Scripts/Game/Rocket.cs
+++ Assets/Scripts/Game/Rocket.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     private Vector2 screenBounds;
-
     private Vector2 rocketSize;
+
+    public float leadDistance = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,19 +30,18 @@ public class Rocket : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
 
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = 0;
 
         float clampedX = Mathf.Clamp(mousePosition.x, -screenBounds.x + rocketSize.x, screenBounds.x - rocketSize.x);
         float clampedY = Mathf.Clamp(mousePosition.y, -screenBounds.y + rocketSize.y, screenBounds.y - rocketSize.y);
 
-        transform.position = Vector3.Lerp(transform.position, new Vector3(clampedX, clampedY, transform.position.z), 0.1f);
 
         Vector2 direction = mousePosition - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        float angle = (Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg) * -1;
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
 
-
-        //transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
-        //transform.position = Vector3.Lerp(transform.position, new Vector3(clampedX, clampedY, transform.position.z), 0.1f);  -- smoooth
+        transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX, clampedY), 0.1f);
     }
 }
